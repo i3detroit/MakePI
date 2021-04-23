de7c10a34455addc903cdc484066ee4ae1830f5c
DJ Meph <norm.witte@gmail.com>
2021-04-23 00:10:59 -0400

    initial commit

---
+++ apps/make-pi/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'make-pi',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/make-pi',
+};
