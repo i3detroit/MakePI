5d923a2470f401dae3a5862a28d2efa26750e659
DJ Meph <norm.witte@gmail.com>
2021-04-25 16:59:03 +0000

    delete unused files

--- apps/make-pi/src/app/app.module.ts
+++ apps/make-pi/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from '@make-pi/shared/auth';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
+  controllers: [AuthController],
 })
 export class AppModule {}
