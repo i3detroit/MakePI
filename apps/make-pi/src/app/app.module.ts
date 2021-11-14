9900d72fcdeb168ea2bb61d9b438bbcc7c7b04cf
DJ Meph <norm.witte@gmail.com>
2021-11-14 02:51:57 +0000

    Generate admin/user controller

--- apps/make-pi/src/app/app.module.ts
+++ apps/make-pi/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from '@make-pi/shared/auth';
+import { UserController } from './admin/user/user.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AuthController],
+  controllers: [AuthController, UserController],
 })
 export class AppModule {}
