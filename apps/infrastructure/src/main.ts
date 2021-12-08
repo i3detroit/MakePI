c05d4c688d5763fe540328f1660eadc673a0f586
DJ Meph <norm.witte@gmail.com>
2021-12-08 05:53:03 +0000

    Create secrets stack with first secret

--- apps/infrastructure/src/main.ts
+++ apps/infrastructure/src/main.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 
 import { environment } from './environments/environment';
+import { PipelinesStack } from './stacks/pipelines/pipelines-stack';
 
 const app = new cdk.App();
 new PipelinesStack(app, 'the-apostles-pipelines', {
--- apps/infrastructure/src/stacks/secrets/secrets-stack.ts
+++ apps/infrastructure/src/stacks/secrets/secrets-stack.ts
@@ -1,8 +1,18 @@
+import { Secret } from '@aws-cdk/aws-secretsmanager';
 import { Construct, Stack } from '@aws-cdk/core';
 import { StackPropsExt } from '../pipelines/pipelines-interface';
 
 export class SecretsStack extends Stack {
   constructor(scope: Construct, id: string, props: StackPropsExt) {
     super(scope, id, props);
+
+    new Secret(this, 'StripeApiKey', {
+      secretName: `STRIPE_API_KEY_${props.envName.toUpperCase()}`,
+      description: 'Stripe API Key',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: 'value',
+      },
+    });
   }
 }
