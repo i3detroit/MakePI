0d55d61e184a8a3bc8a71d8e77fa0d80e7b9bd32
DJ Meph <norm.witte@gmail.com>
2021-12-12 08:35:05 +0000

    Generate Stripe webhooks service

--- libs/shared/stripe/src/lib/shared-stripe.module.ts
+++ libs/shared/stripe/src/lib/shared-stripe.module.ts
@@ -3,6 +3,7 @@ import { SharedSecretsModule } from '@make-pi/shared/secrets';
 import { SecretNames } from '@make-pi/global-config';
 import { StripeCustomersService } from './stripe-customers.service';
 import { StripeChargesService } from './stripe-charges.service';
+import { StripeWebhooksService } from './stripe-webhooks.service';
 
 @Module({
   imports: [
@@ -10,7 +11,11 @@ import { StripeChargesService } from './stripe-charges.service';
       secrets: [SecretNames.SECRET_NAME_STRIPE_API_KEY],
     }),
   ],
-  providers: [StripeCustomersService, StripeChargesService],
+  providers: [
+    StripeCustomersService,
+    StripeChargesService,
+    StripeWebhooksService,
+  ],
   exports: [StripeCustomersService, StripeChargesService],
 })
 export class SharedStripeModule {}
