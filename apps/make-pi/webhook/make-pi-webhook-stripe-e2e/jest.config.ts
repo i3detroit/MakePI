1b88635589e0326b14846e02bdbfa6d7fc9a0263
DJ Meph <norm.witte@gmail.com>
2023-02-05 23:08:57 +0000

    Add make-pi stripe webhook

---
+++ apps/make-pi/webhook/make-pi-webhook-stripe-e2e/jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'make-pi-webhook-make-pi-webhook-stripe-e2e',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../../coverage/make-pi-webhook-make-pi-webhook-stripe-e2e',
+};
---
+++ apps/make-pi/webhook/stripe/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'make-pi-webhook-stripe',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/apps/make-pi/webhook/stripe',
+};
